282314f030531e88542cb9232a77b4f6a9520520
amanda-rice <aricericerice@gmail.com>
2021-07-16 21:33:11 -0600

    cleaned up formatting

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -8,13 +8,10 @@ export default class List {
     this.listId = listId
   }
   get Template() {
-    let check
-
-    this.isChecked ? check = 'checked' : check = ' '
     return `
-    <div class="col-12 d-flex align-items-center"">
-      <input class="px-2" onclick="app.listsController.storeChecked('${this.id}')" type="checkbox" id="task-${this.id}" name="${this.name}" value="${this.name}" ${check}>
-      <label class="px-2 align-self-end flex-fill" for="task-${this.id}">${this.name}</label>
+    <div class="col-12 d-flex align-items-center">
+      <input class="px-2" onclick="app.listsController.storeChecked('${this.id}')" type="checkbox" id="task-${this.id}" name="${this.id}" value="${this.id}" ${this.isChecked ? 'checked' : ' '}>
+      <label class="px-2 align-self-end flex-fill" id="task-${this.id}" for="task-${this.id}">${this.name}</label>
       <i class="p-2 fa fa-trash action" title="delete list" onclick="app.listsController.deleteTask('${this.id}')"></i>
     </div>
   `
